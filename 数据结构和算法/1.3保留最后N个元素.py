2a5828d00a86e6d3c7332b15aae4ab0baa94bd7c
slagmale <781224336@qq.com>
2019-03-13 07:50:42 +0800

    更新1.3队列案例

--- 数据结构和算法/1.3保留最后N个元素.py
+++ 数据结构和算法/1.3保留最后N个元素.py
@@ -17,6 +17,35 @@ def search(lines, pattern, history=5):
         previous_lines.append(line)
 
 
+'''
+写查询元素的代码时，通常使用包含yield表达式的生成器函数
+将搜索过程和使用搜索结果代码解耦
+'''
+
+#  队列案例
+q = deque(maxlen=3)
+q.append(1)
+q.append(2)
+q.append(3)
+print(q)  # 输出deque([1, 2, 3], maxlen=3)
+q.append(4)
+print(q)  # 输出deque([2, 3, 4], maxlen=3)
+# 如果不设定队列大小  得到无限大小的队列，可以在队列两端执行添加和弹出元素的操作
+q2 = deque()
+q2.append(5)
+q2.append(6)
+q2.append(7)
+print(q2)
+q2.appendleft(4)  # 在最左边添加一个元素
+print(q2)
+q2.pop()  # 删除最后一个元素
+print(q2)
+q2.popleft()  # 在最左边删除一个元素
+print(q2)
+
+# 在队列两端插入或删除元素复杂度为O(1)   而在列表开头插入或删除元素复杂度为O(n)
+
+
 # yiled生成式函数案例
 def foo(num):
     print("starting...")
@@ -34,5 +63,3 @@ print(a.__next__())  # 输出res=None 接着while循环返回num=2
 # 第二种
 print(a.__next__())
 print(a.send(5))  # 将值5赋值给了res 然后执行next 输出res=5后接着while循环 返回num
-
-
